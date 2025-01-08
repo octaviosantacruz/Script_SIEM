74261a10d04e27e70468f47270fe90a6b8955f04
Octavio Santacruz <octisantairi@gmail.com>
2025-01-08 17:12:47 -0300

    Agregar manejo de observaciones de inicio de sesión variante en Windows: implementar función para extraer información de logs en un formato alternativo.

--- siem_processor/cases/windows_login.py
+++ siem_processor/cases/windows_login.py
@@ -13,12 +13,34 @@ def handle_windows_login(alarma, cuerpo):
     """
     if alarma == "Notificacion SIEM - Se ha detectado un inicio de sesión":
         observacion = get_windows_login_observation(cuerpo)
+        if observacion == "No se pudo extraer información del inicio de sesión":
+            observacion = get_variant_windows_login_observation(cuerpo)
         is_bold = True if observacion else False
         return observacion, is_bold
 
     return "Caso no clasificado - Añadir manualmente", False
 
+def get_variant_windows_login_observation(cuerpo):
+    """
+    Extrae la información de los logs de Windows Login en un formato variante.
 
+    Args:
+        cuerpo (str): El cuerpo del log.
+
+    Returns:
+        str: Observación extraída o un mensaje de error.
+    """
+    pattern = r'Alarm: Windows-Login-RDP.*?Desde IP: (\d+\.\d+\.\d+\.\d+).*?Hacia Ip: (\d+\.\d+\.\d+\.\d+).*?Usuario: (.*?) Host: (.*?)$'
+    match = re.search(pattern, cuerpo, re.DOTALL)
+
+    if match:
+        ip_origen = match.group(1).strip()
+        ip_destino = match.group(2).strip()
+        user = match.group(3).strip()
+        host = match.group(4).strip()
+        return f"IP Origen: {ip_origen}, IP Destino: {ip_destino}, Usuario: {user}, Host: {host}"
+
+    return "No se pudo extraer información del inicio de sesión variante"
 def get_windows_login_observation(cuerpo):
     """
     Extrae la información de los logs de Windows Login.
